4827ad7961f8c2d63df16c661b0292c7b7f7f05d
Yann Hoffmann <hyann98@gmail.com>
2021-01-16 15:16:00 +0100

    mod

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,8 @@ if ! [ -x "$(command -v ansible)" ]; then
     sudo pacman -S ansible
 fi
 
+export ANSIBLE_NOCOWS=1
+
 # Install pacman plugin for Ansible
 ansible-galaxy collection install community.general
 
@@ -14,5 +16,3 @@ ansible-playbook playbook.yml -i hosts --ask-become-pass
 if command -v terminal-notifier 1> /dev/null 2>&1; then
     terminal-notifier -title "dotfiles: Bootstrap complete" -message "Successfully set up dev environment."
 fi
-
-
